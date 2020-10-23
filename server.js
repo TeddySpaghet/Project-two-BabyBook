ba454fb8736da8f63f430f95658b2b0009465fcb
TeddySpaghet <THEODORE.POCHART@GMAIL.COM>
2020-10-23 11:07:29 -0700

    boilerplate code project2

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use(layouts);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+app.use('/auth', require('./routes/auth'));
+
+var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
+
+module.exports = server;
