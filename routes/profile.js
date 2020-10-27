46fddd62d49e8544883e4fc334c917ede42fd2f4
enbre <noahbrezel@gmail.com>
2020-10-27 15:21:13 -0700

    creates baby profile cards with functional links created via forEach function.

--- routes/profile.js
+++ routes/profile.js
@@ -56,16 +56,6 @@ router.get('/:name', (req, res) => {
         })
 })
 
-// js method of calling modal
-// document.addEventListener('DOMContentLoaded', function() {
-//     let elems = document.querySelectorAll('.modal');
-//     let instances = M.Modal.init(elems, options);
-//   })
-
-// jquery method of calling modal
-// $(document).ready(function () {
-//     $('.modal').modal();
-// })
 
 
 module.exports = router;
